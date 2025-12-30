94e571e0d770bffdbe494b30d0c9af5b3969386f
Abdullah Mahmoud <abdullahm3hmoud@gmail.com>
2025-12-30 19:56:59 +0200

    Quartz sync: Dec 30, 2025, 7:56 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "hxzer0's blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
